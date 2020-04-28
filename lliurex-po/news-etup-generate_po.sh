d16d331470c4b07d3f621949d4b1503a05da72b8
Juan Pelegrina <juapesai@hotmail.com>
2020-04-28 16:29:50 +0200

    Wip in banners

--- lliurex-po/news-etup-generate_po.sh
+++ lliurex-po/news-etup-generate_po.sh
@@ -2,5 +2,5 @@
 
 cp ../lliurex-news-setup/usr/sbin/lliurex-news-setup /tmp/lliurex-news.py
 
-xgettext ../lliurex-news-setup/usr/share/lliurex-news-setup/rsrc/lliurex-news-setup.glade /tmp/lliurex-news.py -o lliurex-news-setup/lliurex-news-setup.pot
+xgettext ../lliurex-news-setup/usr/share/lliurex-news-setup/rsrc/lliurex-news-setup.ui /tmp/lliurex-news.py -o lliurex-news-setup/lliurex-news-setup.pot
 
